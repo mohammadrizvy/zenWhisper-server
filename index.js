5c88c950f83e61cffd00dfcd6cfbc9beaa2ab43a
MD Ehsanul Haque Rizvy <rizvyhq1@gmail.com>
2024-09-15 01:07:43 +0600

    feat : add mongoDB

--- index.js
+++ index.js
@@ -1,18 +1,56 @@
+require("dotenv").config(); // Load environment variables
 const express = require("express");
-const PORT = 5000;
 const app = express();
-
 const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
+app.use(cors());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// Use environment variables for MongoDB username and password
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ss5j1ke.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+}
+
+run().catch(console.dir);
+
+// Express and Socket.IO setup
 const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173", // Frontend address
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  },
+});
 
+app.get("/", (req, res) => {
+  return res.send("Welcome to zenWhisper");
+});
 
-app.get("/" , (req, res)  => {
-    return res.send("Wellcome to zenWhisper")
-}) 
-
-  
-
+const PORT = 5000;
 server.listen(PORT, () => {
-  console.log(`Server is runnig at PORT:${PORT}`);
+  console.log(`Server is running at PORT:${PORT}`);
 });
